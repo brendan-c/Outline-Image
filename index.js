de59e4e3d8fcd3a788071b096f23ddcfba81cdf8
brendan-c <brendan.k.cooney@gmail.com>
2021-09-12 13:29:01 -0400

    Add transparent border on input img to ensure full outline"

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ function chunkArray(array, size) {
 const isEmpty = (colors) => colors[3] === 0
 
 
-const colorPixel = (pixel, neighbors, outlineColor) => {
+const drawOutline = (pixel, neighbors, outlineColor) => {
   let result = []
 
   const hasNeighbor = neighbors.some(
@@ -69,8 +69,18 @@ const colorPixel = (pixel, neighbors, outlineColor) => {
 
 function outlineImage(img, outputPath, outlineColor = [255, 250, 226, 255]) {
   // Break up array of pixels into rows
-  const pixels = chunkArray(img.data.pixels, img.data.width)
+  let pixels = chunkArray(img.data.pixels, img.data.width)
 
+  // Create transparent border to ensure full outline
+  pixels = pixels.map(row => [[0,0,0,0], ...row, [0,0,0,0]])
+  pixels.unshift(Array(pixels[0].length).fill([0,0,0,0]))
+  pixels.push(Array(pixels[0].length).fill([0,0,0,0]))
+
+  // New height/width with added transparent border
+  let height = pixels.length
+  let width = pixels[0].length
+
+ 
   // Resulting array of pixels including the outline
   let result = []
 
@@ -80,19 +90,19 @@ function outlineImage(img, outputPath, outlineColor = [255, 250, 226, 255]) {
     // Get neighboring pixels. If the pixel is on outer border, 
     // the non-existing neighbor gets value of False
       let above = i === 0 ? false : pixels[i - 1][col]
-      let below = i === img.data.width - 1 ? false : pixels[i + 1][col]
-      let right = col === img.data.width - 1 ? false : pixels[i][col + 1]
+      let below = i === height - 1 ? false : pixels[i + 1][col]
+      let right = col === width - 1 ? false : pixels[i][col + 1]
       let left = col === 0 ? false : pixels[i][col - 1]
 
       neighbors = [above, below, right, left]
 
-      result.push(colorPixel(pixel, neighbors, outlineColor))
+      result.push(...drawOutline(pixel, neighbors, outlineColor))
     })
   })
 
   return new Promise(function (resolve, reject) {
     // Creates a new image with empty pixels based on input image dimensions
-    new jimp(img.data.width, img.data.height, function (err, image) {
+    new jimp(width, height, function (err, image) {
       if (err) return reject(err)
       // Counter, used to sync with the result array of pixels
       let i = 0
@@ -106,13 +116,13 @@ function outlineImage(img, outputPath, outlineColor = [255, 250, 226, 255]) {
         image.bitmap.height,
         function (x, y, index) {
           // Red
-          this.bitmap.data[index + 0] = result[i][0][0]
+          this.bitmap.data[index + 0] = result[i][0]
           // Green
-          this.bitmap.data[index + 1] = result[i][0][1]
+          this.bitmap.data[index + 1] = result[i][1]
           // Blue
-          this.bitmap.data[index + 2] = result[i][0][2]
+          this.bitmap.data[index + 2] = result[i][2]
           // Alpha
-          this.bitmap.data[index + 3] = result[i][0][3]
+          this.bitmap.data[index + 3] = result[i][3]
           i += 1
         }
       )
